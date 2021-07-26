fce3e653bef8df1857fd121b04bdf2f60deff759
Leonel Matos <leonel.amatos@gmail.com>
2021-07-26 11:21:43 -0400

    Initial component setup

---
+++ src/components/Editor.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+const Editor = () => {
+	return (
+		<div id='Editor'>
+			<h1>Editor Window</h1>
+			<textarea name='editorText' id='editor-area'></textarea>
+		</div>
+	);
+};
+
+export default Editor;
