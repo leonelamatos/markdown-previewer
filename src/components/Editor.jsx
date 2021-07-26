e3058374a800d09c91b1ed734ebffd233a44b469
Leonel Matos <leonel.amatos@gmail.com>
2021-07-26 16:50:56 -0400

    Imports the Marked pacakes and set the logic to display markdown

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,10 +1,41 @@
-// import React from 'react';
+import { useState, useEffect } from 'react';
+import Preview from './Preview';
+import marked from 'marked';
+import DOMPurify from 'dompurify';
 
 const Editor = () => {
+	const [text, setText] = useState('This is some text');
+
+	useEffect(() => {
+		const textArea = document.querySelector('#editor');
+		const output = document.querySelector('#preview-area');
+		textArea.addEventListener('scroll', function () {
+			console.log(this.scrollTop);
+
+			document.querySelector('#preview-area').scrollTop =
+				this.scrollTop * 1.3;
+		});
+	}, []);
+
+	function onChangeHandler(e) {
+		const { value } = e.target;
+		const markedDown = DOMPurify.sanitize(marked(value));
+		setText(markedDown);
+	}
+
 	return (
-		<div id='Editor'>
-			<h1>Editor Window</h1>
-			<textarea name='editorText' id='editor-area'></textarea>
+		<div className='container'>
+			<div id='Editor'>
+				<h2>Editor Window</h2>
+				<div>
+					<textarea
+						name='editorText'
+						id='editor'
+						onChange={onChangeHandler}></textarea>
+				</div>
+			</div>
+
+			<Preview text={text} />
 		</div>
 	);
 };
