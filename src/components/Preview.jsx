10b7ba57e0b917d0e1215948f9cc6f731a5e1f7f
Leonel Matos <leonel.amatos@gmail.com>
2021-07-26 16:51:28 -0400

    Adds the innerHTLM text to the div

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const Preview = () => {
+const Preview = ({ text }) => {
 	return (
 		<div id='Preview'>
-			<h1>Preview Window</h1>
-			<div id='preview-area'></div>
+			<h2>Preview Window</h2>
+			<div id='preview-area' dangerouslySetInnerHTML={{ __html: text }}>
+				{/* {text} */}
+			</div>
 		</div>
 	);
 };
