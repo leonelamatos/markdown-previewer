e00181baa91ca65ade15f5c5388d217ee7566192
Leonel Matos <leonel.amatos@gmail.com>
2021-07-26 16:50:04 -0400

    Moves the Preview component inside the Editor component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Editor from './components/Editor';
-import Preview from './components/Preview';
+// import Preview from './components/Preview';
 
 import './assets/css/main.min.css';
 
 function App() {
 	return (
 		<div className='App'>
+			<h1>Markdown Previewer</h1>
 			<Editor />
-			<Preview />
+			{/* <Preview /> */}
 		</div>
 	);
 }
